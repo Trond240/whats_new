0edc2ef96b53f3d6aed9a7f1a2c193600de5a872
Trond Makonese <trondation@gmail.com>
2020-03-18 19:07:46 -0600

    Creat menu bar.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,4 +3,18 @@ import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
 
-export default Menu;
+export const Menu = (props) => {
+  console.log(props)
+  return (
+    <nav>
+      <h1>What's <span className='new'>New?</span></h1>
+      <ul className='menu-bar'>
+        <li className='nav-category' onClick={() => props.changeNews('local')}>Local News</li>
+        <li className='nav-category' onClick={() => props.changeNews('technology')}>Technology</li>
+        <li className='nav-category' onClick={() => props.changeNews('entertainment')}>Entertainment</li>
+        <li className='nav-category' onClick={() => props.changeNews('science')}>Science</li>
+        <li className='nav-category' onClick={() => props.changeNews('health')}>Health</li>
+      </ul>
+    </nav>
+  )
+}
