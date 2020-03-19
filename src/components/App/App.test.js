9552a3c5ab048caaf0356532a5ae0aaccfe4a959
Trond Makonese <trondation@gmail.com>
2020-03-19 16:53:30 -0600

    Need to fix app test and create form for search, also need to fix the links.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App', () => {
+  it('show different articles', () => {
+
+    const { getByText } = render(<App />);
+
+    fireEvent.click(getByText('Local News'));
+    fireEvent.click(getByText('Technology'));
+    fireEvent.click(getByText('Entertainment'));
+    fireEvent.click(getByText('Science'));
+    fireEvent.click(getByText('Health'));
+
+
+
+    expect(fakeRun).toBeCalled();
+  });
 });
